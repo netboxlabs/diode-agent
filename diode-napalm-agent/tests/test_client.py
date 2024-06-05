f3c862938481d2e553db8168034c21733a9e7c8b
Leonardo Parente <23251360+leoparente@users.noreply.github.com>
2024-06-05 13:08:41 +0000

    Add more unit tests

---
+++ diode-napalm-agent/tests/test_client.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# Copyright 2024 NetBox Labs Inc
+"""Unit tests for Client class in client.py."""
+
+import pytest
+from unittest.mock import patch, MagicMock
+
+from diode_napalm.client import Client
+from diode_napalm.translate import translate_data
+
+
+@pytest.fixture
+def sample_data():
+    """Sample data for testing ingestion."""
+    return {
+        "device": {
+            "hostname": "router1",
+            "model": "ISR4451",
+            "vendor": "Cisco",
+            "serial_number": "123456789",
+            "site": "New York",
+            "driver": "ios"
+        },
+        "interface": {
+            "GigabitEthernet0/0": {
+                "is_enabled": True,
+                "mtu": 1500,
+                "mac_address": "00:1C:58:29:4A:71",
+                "speed": 1000,
+                "description": "Uplink Interface"
+            }
+        },
+        "interface_ip": {
+            "GigabitEthernet0/0": {
+                "ipv4": {
+                    "192.0.2.1": {
+                        "prefix_length": 24
+                    }
+                }
+            }
+        },
+        "driver": "ios",
+        "site": "New York"
+    }
+
+
+@patch('diode_napalm.client.version_semver', return_value="0.0.0")
+@patch('diode_napalm.client.DiodeClient')
+def test_init_client(mock_diode_client_class, mock_version):
+    """Test the initialization of the Diode client."""
+    client = Client()
+    client.init_client(target="https://example.com",
+                       api_key="dummy_api_key", tls_verify=True)
+
+    mock_diode_client_class.assert_called_once_with(
+        target="https://example.com", app_name="diode-napalm-agent", app_version=mock_version(), api_key="dummy_api_key", tls_verify=True
+    )
+
+
+@patch('diode_napalm.client.DiodeClient')
+def test_ingest_success(mock_diode_client_class, sample_data):
+    """Test successful data ingestion."""
+    client = Client()
+    client.init_client(target="https://example.com",
+                       api_key="dummy_api_key", tls_verify=True)
+
+    mock_diode_instance = mock_diode_client_class.return_value
+    mock_diode_instance.ingest.return_value.errors = []
+
+    with patch('diode_napalm.client.translate_data', return_value=translate_data(sample_data)) as mock_translate_data:
+        client.ingest(sample_data)
+        mock_translate_data.assert_called_once_with(sample_data)
+        mock_diode_instance.ingest.assert_called_once()
+
+
+@patch('diode_napalm.client.DiodeClient')
+def test_ingest_failure(mock_diode_client_class, sample_data):
+    """Test data ingestion with errors."""
+    client = Client()
+    client.init_client(target="https://example.com",
+                       api_key="dummy_api_key", tls_verify=True)
+
+    mock_diode_instance = mock_diode_client_class.return_value
+    mock_diode_instance.ingest.return_value.errors = ["Error1", "Error2"]
+
+    with patch('diode_napalm.client.translate_data', return_value=translate_data(sample_data)) as mock_translate_data:
+        client.ingest(sample_data)
+        mock_translate_data.assert_called_once_with(sample_data)
+        mock_diode_instance.ingest.assert_called_once()
+
+    assert len(mock_diode_instance.ingest.return_value.errors) > 0
+
+
+def test_ingest_without_initialization():
+    """Test ingestion without client initialization raises ValueError."""
+    client = Client()
+    with pytest.raises(ValueError, match="Diode client not initialized"):
+        client.ingest({})
---
+++ diode-napalm-agent/tests/test_translate.py
@@ -0,0 +1,98 @@
+import pytest
+from diode_napalm.translate import translate_device, translate_interface, translate_interface_ips, translate_data
+from netboxlabs.diode.sdk.ingester import Device, DeviceType, Entity, Interface, IPAddress, Platform, Prefix
+
+
+@pytest.fixture
+def sample_device_info():
+    """Sample device information for testing."""
+    return {
+        "hostname": "router1",
+        "model": "ISR4451",
+        "vendor": "Cisco",
+        "serial_number": "123456789",
+        "site": "New York",
+        "driver": "ios",
+        "interface_list": ["GigabitEthernet0/0"],
+    }
+
+
+@pytest.fixture
+def sample_interface_info():
+    """Sample interface information for testing."""
+    return {
+        "GigabitEthernet0/0": {
+            "is_enabled": True,
+            "mtu": 1500,
+            "mac_address": "00:1C:58:29:4A:71",
+            "speed": 1000,
+            "description": "Uplink Interface"
+        }
+    }
+
+
+@pytest.fixture
+def sample_interfaces_ip():
+    """Sample interface IPs for testing."""
+    return {
+        "GigabitEthernet0/0": {
+            "ipv4": {
+                "192.0.2.1": {
+                    "prefix_length": 24
+                }
+            }
+        }
+    }
+
+
+def test_translate_device(sample_device_info):
+    """Ensure device translation is correct."""
+    device = translate_device(sample_device_info)
+    assert device.name == "router1"
+    assert device.device_type.model == "ISR4451"
+    assert device.platform.name == "ios"
+    assert device.serial == "123456789"
+    assert device.site.name == "New York"
+
+
+def test_translate_interface(sample_device_info, sample_interface_info):
+    """Ensure interface translation is correct."""
+    device = translate_device(sample_device_info)
+    interface = translate_interface(
+        device, "GigabitEthernet0/0", sample_interface_info["GigabitEthernet0/0"])
+    assert interface.device.name == "router1"
+    assert interface.name == "GigabitEthernet0/0"
+    assert interface.enabled is True
+    assert interface.mtu == 1500
+    assert interface.mac_address == "00:1C:58:29:4A:71"
+    assert interface.speed == 1000
+    assert interface.description == "Uplink Interface"
+
+
+def test_translate_interface_ips(sample_device_info, sample_interface_info, sample_interfaces_ip):
+    """Ensure interface IPs translation is correct."""
+    device = translate_device(sample_device_info)
+    interface = translate_interface(
+        device, "GigabitEthernet0/0", sample_interface_info["GigabitEthernet0/0"])
+    ip_entities = list(translate_interface_ips(
+        interface, sample_interfaces_ip))
+    assert len(ip_entities) == 2
+    assert ip_entities[0].prefix.prefix == "192.0.2.0/24"
+    assert ip_entities[1].ip_address.address == "192.0.2.1/24"
+
+
+def test_translate_data(sample_device_info, sample_interface_info, sample_interfaces_ip):
+    """Ensure data translation is correct."""
+    data = {
+        "device": sample_device_info,
+        "interface": sample_interface_info,
+        "interface_ip": sample_interfaces_ip,
+        "driver": "ios",
+        "site": "New York"
+    }
+    entities = list(translate_data(data))
+    assert len(entities) == 4
+    assert entities[0].device.name == "router1"
+    assert entities[1].interface.name == "GigabitEthernet0/0"
+    assert entities[2].prefix.prefix == "192.0.2.0/24"
+    assert entities[3].ip_address.address == "192.0.2.1/24"
